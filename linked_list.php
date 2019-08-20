a9f16595d0aecd71a4e52f615c9ecf9cf3394158
kirbykirbydev <47863460+kirbykirbydev@users.noreply.github.com>
2019-08-21 01:01:01 +0800

    Update linked_list.php
    fixed PHP comment

--- linked_list.php
+++ linked_list.php
@@ -8,16 +8,15 @@
  *  https://www.quora.com/When-would-you-use-a-linked-list
  *  
  *  Linked lists are preferable over arrays when:
-
-    a) you need constant-time insertions/deletions from the list (such as in real-time computing where time predictability is absolutely critical)
-    
-    b) you don't know how many items will be in the list. With arrays, you may need to re-declare and copy memory if the array grows too big
-    
-    c) you don't need random access to any elements
-    
-    d) you want to be able to insert items in the middle of the list (such as a priority queue)
-
-
+ *
+ *  a) you need constant-time insertions/deletions from the list (such as in real-time computing where time predictability is absolutely critical)
+ *  
+ *  b) you don't know how many items will be in the list. With arrays, you may need to re-declare and copy memory if the array grows too big
+ *  
+ *  c) you don't need random access to any elements
+ *  
+ *  d) you want to be able to insert items in the middle of the list (such as a priority queue)
+ *
  *  also here: https://stackoverflow.com/questions/393556/when-to-use-a-linked-list-over-an-array-array-list
  * 
  *  
