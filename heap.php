eba2184e908628f35e58dc64d7388843d150a491
kirby <kirby@kirby-chocolate-mousse>
2019-08-20 22:48:55 +0800

    added more descriptions to heap

--- heap.php
+++ heap.php
@@ -5,6 +5,23 @@
  *PHP heap api documentation can be found at
  *https://www.php.net/manual/en/class.splmaxheap.php
  * 
+ * 
+ * why use heaps ?
+ * In summary , it can be a container of a sorted list of items. While items are inserted into
+ * a heap, it automatically adds the item in a sorted location.
+ * 
+ * in the end of an insert process, the max (or min) value is always at the top and is ready to be fetched
+ * 
+ * 
+ * this is MORE EFFICIENT than using an array to dump all the data and then trying to sort 
+ * to determine which is the largest (or smallest value).
+ * 
+ * 
+ * why use SplHeap ?
+ * PHP has a built-in HEAP data structure that is ready to use. It is already compiled 
+ * and is naturally faster than coding a heap manually 
+ * 
+ * 
  */
 
 
@@ -36,7 +53,7 @@ class SampleMaxHeap extends SplHeap
 }
 
 /**
- * I am using two heaps here FOR EXAMPLE ONLY
+ * I am using two heaps here AS EXAMPLE ONLY (you just need one heap for one purpose)
  * the idea is to REVERSE the comparison in the inherited function 
  */
 
